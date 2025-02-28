368061d5ec0f9a71668d75569c66caece7a53e8c
Nattewut <tay231218@gmail.com>
2025-02-28 19:48:39 +0700

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	#Leave as empty string for base url
-	path('', views.store, name="store"),
-	path('cart/', views.cart, name="cart"),
-	path('checkout/', views.checkout, name="checkout"),
- 	path('generate_qr/', views.generate_qr, name='generate_qr'),  # เพิ่มเส้นทางสำหรับการสร้าง QR Code
-	path('update_item/', views.updateItem, name="update_item"),
-	path('process_order/', views.processOrder, name="process_order"),
-
+    # Leave as empty string for base url
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),  # ✅ เพิ่ม API สำหรับ Stripe Checkout
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+    path('success/', views.success, name='success'),  # ✅ เพิ่มเส้นทางสำหรับหน้าสำเร็จ
+    path('cancel/', views.cancel, name='cancel'),  # ✅ เพิ่มเส้นทางสำหรับหน้าล้มเหลว
 ]
