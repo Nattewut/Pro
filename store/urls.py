e85d54c8fbadc1f345247d13f197e1baf47d8251
Nattewut <tay231218@gmail.com>
2025-03-05 22:08:30 +0700

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('process_order/', views.processOrder, name="process_order"),
     path('control_motor/<int:motor_number>/', views.control_motor, name="control_motor"),
     # ✅ รองรับทั้ง JSON API และ HTML Page
-    path('success/', views.success, name='success'),
-    path('cancel/', views.cancel, name='cancel'),
+    #path('success/', views.success, name='success'),
+    #path('cancel/', views.cancel, name='cancel'),
 ]
