db49f486563425cab61382830c26b31e2c25592c
Nattewut <tay231218@gmail.com>
2025-03-05 17:55:23 +0700

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),
     path('update_item/', views.updateItem, name="update_item"),
     path('process_order/', views.processOrder, name="process_order"),
-    
+    path('control_motor/<int:motor_number>/', views.control_motor, name="control_motor"),
     # ✅ รองรับทั้ง JSON API และ HTML Page
     path('success/', views.success, name='success'),
     path('cancel/', views.cancel, name='cancel'),
